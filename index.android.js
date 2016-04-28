d38d6b9b35637d9379c7a181016a3170f087e34b
twillzy <william.tanzy@gmail.com>
2016-04-28 17:36:05 +1000

    Button text and screen color toggle when clicked

--- index.android.js
+++ index.android.js
@@ -6,31 +6,30 @@ import React, {
   View
 } from 'react-native';
 
-var MK = require('react-native-material-kit');
-var {
+import MK, {
   MKButton,
-  MKColor,
-} = MK;
+} from 'react-native-material-kit';
 
 class senses extends Component {
 
   constructor(props) {
-    super(props); 
+    super(props);
     this.state = {
       isCurrentlySensing: false,
     };
   }
 
   render() {
-    var text = (this.state.isCurrentlySensing) ? "STOP SENSING" : "START SENSING";
+    let text = (this.state.isCurrentlySensing) ? "STOP SENSING" : "START SENSING";
+    let backgroundColor = (this.state.isCurrentlySensing) ? "#58E2C2" : "#4E92DF";
 
     return (
-      <View style={styles.container}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: backgroundColor}}>
         <MKButton
           backgroundColor="white"
           borderRadius={4}
           padding={15}
-          onPress={this._handlePress}
+          onPress={this.handlePress.bind(this)}
           >
           <Text pointerEvents="none"
                 style={{color: '#66E5C8', fontWeight: 'bold',}}>
@@ -41,18 +40,9 @@ class senses extends Component {
     );
   }
 
-  _handlePress(event) {
-    console.warn("clicked!");
+  handlePress(event) {
+    this.setState({isCurrentlySensing: !this.state.isCurrentlySensing});
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4E92DF',
-  }
-});
 
 AppRegistry.registerComponent('senses', () => senses);
