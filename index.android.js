f5d0a573213024c59eddff0d7e54d71d2e33bd2b
twillzy <william.tanzy@gmail.com>
2016-04-29 15:22:24 +1000

    Refactored index.android.js

--- index.android.js
+++ index.android.js
@@ -1,47 +1,15 @@
 import React, {
   AppRegistry,
   Component,
-  StyleSheet,
-  Text,
-  View
 } from 'react-native';
 
-import MK, {
-  MKButton,
-} from 'react-native-material-kit';
+import Home from './App/Views/home';
 
 class senses extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      isCurrentlySensing: false,
-    };
-  }
-
   render() {
-    let text = (this.state.isCurrentlySensing) ? "STOP SENSING" : "START SENSING";
-    let backgroundColor = (this.state.isCurrentlySensing) ? "#58E2C2" : "#4E92DF";
-
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: backgroundColor}}>
-        <MKButton
-          backgroundColor="white"
-          borderRadius={4}
-          padding={15}
-          onPress={this.handlePress.bind(this)}
-          >
-          <Text pointerEvents="none"
-                style={{color: '#66E5C8', fontWeight: 'bold',}}>
-            {text}
-          </Text>
-        </MKButton>
-      </View>
+      <Home/>
     );
-  }
-
-  handlePress(event) {
-    this.setState({isCurrentlySensing: !this.state.isCurrentlySensing});
   }
 }
 
