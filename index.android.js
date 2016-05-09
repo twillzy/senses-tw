772ecb01a8196b4ac68e9a587fb3a0fc057036a8
sreeja <sreejagpt@gmail.com>
2016-05-09 13:55:08 +1000

    [will/sreeja]Started navigator routing in react frontend

--- index.android.js
+++ index.android.js
@@ -1,15 +1,42 @@
 import React, {
   AppRegistry,
   Component,
+  Navigator,
+  BackAndroid,
 } from 'react-native';
 
 import Home from './App/Views/home';
 
-class senses extends Component {
-  render() {
+var _navigator;
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (_navigator && _navigator.getCurrentRoutes().length > 1) {
+    _navigator.pop();
+    return true;
+  }
+  return false;
+});
+
+
+var RouteMapper = function(route, navigationOperations, onComponentRef) {
+  _navigator = navigationOperations;
+  if (route.name === 'home') {
     return (
       <Home/>
     );
+  } 
+};
+
+class senses extends Component {
+  render() {
+    var initialRoute = {name: 'home'};
+
+    return (
+	  <Navigator
+	    initialRoute={initialRoute}
+	    configureScene={() => Navigator.SceneConfigs.FadeAndroid}
+	    renderScene={RouteMapper}
+	    />
+	);
   }
 }
 
