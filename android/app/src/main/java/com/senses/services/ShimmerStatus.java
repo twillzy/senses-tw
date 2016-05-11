986b69fec8dea97acbe62f1238b37362619ea45e
sreeja <sreejagpt@gmail.com>
2016-05-11 17:28:21 +1000

    refactored enum

--- android/app/src/main/java/com/senses/services/ShimmerStatus.java
+++ android/app/src/main/java/com/senses/services/ShimmerStatus.java
@@ -1,15 +1,9 @@
 package com.senses.services;
 
 public enum ShimmerStatus {
-    DISCONNECTED(-1),
-    CONNECTING(0),
-    CONNECTED(1),
-    READY_TO_STREAM(2),
-    STREAMING(3);
-
-    private final int value;
-
-    ShimmerStatus(int value) {
-        this.value = value;
-    }
+    DISCONNECTED,
+    CONNECTING,
+    CONNECTED,
+    READY_TO_STREAM,
+    STREAMING;
 }
