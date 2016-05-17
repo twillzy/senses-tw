ae9683237eb5a34a620dd7bc992cc1a7ded2e54f
sreeja <sreejagpt@gmail.com>
2016-05-17 14:38:34 +1000

    [sreeja] removed ToastModule

--- android/app/src/main/java/com/senses/ShimmerReactPackage.java
+++ android/app/src/main/java/com/senses/ShimmerReactPackage.java
@@ -6,7 +6,6 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.senses.reactmodules.ConnectToHardwareModule;
-import com.senses.reactmodules.ToastModule;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -28,7 +27,6 @@ class ShimmerReactPackage implements ReactPackage {
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
 
       List<NativeModule> modules = new ArrayList<>();
-      modules.add(new ToastModule(reactContext));
       modules.add(new ConnectToHardwareModule(reactContext));
       return modules;
   }
