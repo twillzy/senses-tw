81a9a49062a2f0660ff2b240d6c1ad1f30d8a0ac
twillzy <william.tanzy@gmail.com>
2016-06-24 22:44:31 +1000

    timer and front camera added

--- App/Views/camerasenses.js
+++ App/Views/camerasenses.js
@@ -8,6 +8,7 @@ import React, {
 
 import Camera from 'react-native-camera';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
 import ConnectToHardwareModule from './../../App/Modules/ConnectToHardwareModule';
 
 export default class CameraSenses extends Component {
@@ -17,7 +18,11 @@ export default class CameraSenses extends Component {
     this.state = {
       videoUri: null,
       startRecording: true,
-      iconName: "fiber-manual-record"
+      iconName: "fiber-manual-record",
+      cameraType: "back",
+      timeLapse: 0,
+      timerID: 0,
+      timer: "00:00:00"
     };
   }
 
@@ -31,19 +36,75 @@ export default class CameraSenses extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.timerCameraContainer}>
+          <View style={styles.timerContainer}>
+            <Text style={styles.timerText}>{this.state.timer}</Text>
+          </View>
+          <FAIcon.Button name="camera"
+                         size={30}
+                         color="white"
+                         backgroundColor="black"
+                         borderRadius={0}
+                         iconStyle={styles.cameraButton}
+                         onPress={this.switchCamera.bind(this)}>
+          </FAIcon.Button>
+        </View>
         <Camera ref="camera"
                 aspect={Camera.constants.Aspect.Fill}
+                type={this.state.cameraType}
                 style={styles.preview}/>
         <Icon.Button name={this.state.iconName}
-              size={100}
-              color="red"
-              backgroundColor="black"
-              borderRadius={0}
-              iconStyle={styles.recordButton}
-              onPress={this.record.bind(this)}>
+                     size={100}
+                     color="red"
+                     backgroundColor="black"
+                     borderRadius={0}
+                     iconStyle={styles.recordButton}
+                     onPress={this.record.bind(this)}>
         </Icon.Button>
       </View>
     );
+  }
+
+  convertTime(seconds) {
+    let hr = 0;
+    let min = 0;
+    let sec = 0;
+
+    hr = parseInt(seconds / 3600);
+    min = parseInt((seconds % 3600) / 60);
+    sec = (seconds % 3600) % 60;
+
+    if (hr < 10) hr = "0" + hr.toString();
+    if (min < 10) min = "0" + min.toString();
+    if (sec < 10) sec = "0" + sec.toString();
+
+    return hr.toString() + ":" + min.toString() + ":" + sec.toString();
+  }
+
+  startTimer() {
+    let timerID = setInterval(() => {
+      this.setState({timeLapse: this.state.timeLapse + 1});
+      this.setState({timer: this.convertTime(this.state.timeLapse)});
+    }, 1000);
+
+    this.setState({timerID: timerID});
+  }
+
+  stopTimer() {
+    clearInterval(this.state.timerID);
+  }
+
+  switchCamera() {
+    switch (this.state.cameraType) {
+      case "front":
+        this.setState({cameraType: "back"});
+        break;
+      case "back":
+        this.setState({cameraType: "front"});
+        break;
+      default:
+        break;
+    }
   }
 
   record() {
@@ -59,16 +120,19 @@ export default class CameraSenses extends Component {
               this.setState({...this.state, videoUri: data});
             })
             .catch(console.log);
+          this.startTimer();
           this.setState({startRecording: false, iconName: "stop"});
         }
       });
     } else {
+      this.stopTimer();
       stopStreaming().then((streamingStatus) => {
         if (streamingStatus === 'stopped') {
           this.refs.camera.stopCapture({
             })
             .then(data => {
               console.log(data);
+              this.stopTimer();
               this.navigate('results');
             })
             .catch(console.log);
@@ -114,5 +178,18 @@ const styles = StyleSheet.create({
   recordButton: {
     marginLeft: Dimensions.get('window').width / 2 - 60,
     marginVertical: -15
+  },
+  timerCameraContainer: {
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  timerText: {
+    color: 'white',
+    fontSize: 20,
+    marginLeft: 10
+  },
+  timerContainer: {
+    justifyContent: 'center'
   }
 });
